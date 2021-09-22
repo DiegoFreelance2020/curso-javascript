7afb22e9e1e538d6ad6393fb1fab1e0c73a39733
Diego freelance <diego.burgos.mesa95@gmail.com>
2021-09-21 19:23:35 -0500

    Inicio curso

---
+++ js/main.js
@@ -0,0 +1,81 @@
+// Comentario de una sola linea
+/*
+Este es un coemntario
+multilinea
+*/
+// alert("Funciona")
+
+/*
+=============VARIABLES================
+*/
+
+//Tipo String
+var mivariablestring = "SOY EL RESULTADO";
+let mivariablelet = "SOY 'VARIABLE' TIPO LET";
+const nombre = "Alberto";
+const apellido = "Gomez";
+
+// Concatenar strings
+let nombrecompleto = nombre + " " + apellido;
+let nombrecompleto2 = `${nombre} ${apellido}`
+
+//Tipo entero
+var numeroCamiseta = 1920
+
+//Tipo Decimal
+var tallaZapatos = 38.5
+
+let numeroTotal = numeroCamiseta + tallaZapatos;
+
+
+//Tipo boolean
+var estadoLuz = true
+var estadoEstufa = false
+
+//Tipo Array
+var frutas = ["manzana", "banana", "pera", 24, true]
+var diasSemana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+
+//Tipo Objeto o tupla
+var persona = {
+    nombre: "Juan",
+    apellido: "Gomez",
+    edad: 24,
+    soltero: true
+}
+
+//Imprimir en el DOM
+//document.getElementById("nombre").innerHTML = persona.nombre
+
+/*
+=============FUNCIONES================
+*/
+
+function suma(numero1, numero2){
+    var resultado = numero1 + numero2
+
+    return resultado
+}
+
+function mostrarNombre(){
+    return persona.apellido + persona.edad
+}
+
+var miSuma = suma(4, 20)
+
+console.log(mostrarNombre())
+
+function resta(numero1, numero2){
+    var resultado = numero1 - numero2
+
+    return resultado
+}
+
+var miResta = resta(5, 14)
+
+console.log(miResta)
+
+//Funcion tipo flecha
+const nuevaSuma = (a, b) => (a + b)
+
+console.log(nuevaSuma(5, 90))
